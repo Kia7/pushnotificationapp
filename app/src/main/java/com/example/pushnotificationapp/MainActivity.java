f240b03184fed372b9bc32459bb6c0cfb6ec9fae
Suryam <suryam.srivastava@sap.com>
2019-06-28 00:05:10 +0530

    First Commit

---
+++ app/src/main/java/com/example/pushnotificationapp/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.pushnotificationapp;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
+import static android.content.ContentValues.TAG;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+//                            Log.w(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+
+                        // Log and toast
+//                        String msg = getString("Token generated", token);
+//                        Log.d(TAG, msg);
+                        Log.d(TAG, "Refreshed Token: " + token);
+                        Toast.makeText(MainActivity.this, token.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+        // [END retrieve_current_token]
+    }
+}
